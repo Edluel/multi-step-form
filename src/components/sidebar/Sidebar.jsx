ccad536d1b8072fda6ad47178251e7f0bf05d7e7
Edluel <edluel_machado@hotmail.com>
2023-03-01 23:08:40 -0300

    creating side-bar component

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className="side-bar">
+        <div className="side-bar-step">
+            <button>1</button>
+        </div>
+        <div className="side-bar-step">
+            <button>2</button>
+        </div>
+        <div className="side-bar-step">
+            <button>3</button>
+        </div>
+        <div className="side-bar-step">
+            <button>4</button>
+        </div>
+    </div>
+  )
+}
